65c9b1275e32803c55d82dcc6c0295053c7b50ec
LondaNH <117847738+LondaNH@users.noreply.github.com>
2023-05-09 21:16:50 -0400

    updated files

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -21,13 +21,13 @@ const AppNavbar = () => {
           <Navbar.Collapse id='navbar' className='d-flex flex-row-reverse'>
             <Nav className='ml-auto d-flex'>
               <Nav.Link as={Link} to='/'>
-                Search For Books
+                Search For Music
               </Nav.Link>
               {/* if user is logged in show saved books and logout */}
               {Auth.loggedIn() ? (
                 <>
                   <Nav.Link as={Link} to='/saved'>
-                    See Your Books
+                    Saved Playlist
                   </Nav.Link>
                   <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
                 </>
