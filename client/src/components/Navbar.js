49e9242612c9d4c2e9d2c046ada2fee400a30e77
Joy Johns <JRJ20716@gmail.com>
2023-05-09 21:29:47 -0400

    removed import

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaUmbrellaBeach } from 'react-icons/fa'
 import '../styles/Navbar.css'
 // import SignUpForm from './SignupForm';
 // import LoginForm from './LoginForm';
