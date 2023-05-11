eb2d8c7a445bde6ee30b9cc6c47eeb914149d689
Beatriz Rivard <beatrizfreire182@gmail.com>
2023-05-11 15:45:02 -0400

    loginform.js and signupform.js is done but need few more adjust

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,6 +3,8 @@ import { useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth';
+import '../styles/LoginForm.css';
+
 
 
 function Login(props) {
@@ -29,42 +31,84 @@ function Login(props) {
       [name]: value,
     });
   };
-
+  function Login() {
+    const [email, setEmail ] = useState ("")
+  }
+  
   return (
-    <div className="container my-1">
-      <Link to="/signup">← Head to Sign Up Page</Link>
+    
+    <div className="container-my-1">
+      {/* <Link to="/signup">← Head to Sign Up Page</Link> */}
+      {/* <form onSubmit={handleFormSubmit}> */}
+        <div className="flex-row-space-between-my-4"> 
+        <div className="wrap-login">
+        <form className="login-form">
+        <span className="login-form-title">Login</span>
 
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-4">
-          <label htmlFor="email">Email address:</label>
-          <input
-            placeholder="youremail@test.com"
+        <div className="wrap-input">
+          <input className="input"
+           placeholder="youremail@test.com"
             name="email"
             type="email"
             id="email"
             onChange={handleChange}
-          />
+            />
+          <span className="focus-input" data-placeholder="Email"></span>
         </div>
-        <div className="flex-row space-between my-4">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
+
+        <div className="wrap-input">
+          <input className="input" 
+          placeholder="******"
             name="password"
             type="password"
             id="pwd"
-            onChange={handleChange}
-          />
+            onChange={handleChange}/>
+          <span className="focus-input" data-placeholder="Password"></span>
         </div>
         {error ? (
           <div>
             <p className="error-text">Oops, wrong credentials!</p>
           </div>
         ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
+
         </form>
+        <div className="container-form-login-btn">
+          <button className="submit">Enter</button>
+        </div>
+        <div className="text-info">
+          <span className="txt1">Don't have an account yet?</span>
+          <a className="txt2" href="#">Create Account</a>
+        </div>
+        </div>
+
+          {/* <label htmlFor="email">Email:</label> */}
+          {/* changes made below 1*/}
+          {/* <input className= "input-box"
+            placeholder="youremail@test.com"
+            name="email"
+            type="email"
+            id="email"
+            onChange={handleChange}
+          /> */}
+        </div>
+        {/* <div className="flex-row-space-between-my-4">
+          <label htmlFor="pwd">Password:</label>
+          changes made bellow 1
+          <input className= "input-box"
+            placeholder="******"
+            name="password"
+            type="password"
+            id="pwd"
+            onChange={handleChange}
+          />
+        </div> */}
+        {/* {error ? (
+          <div>
+            <p className="error-text">Oops, wrong credentials!</p>
+          </div>
+        ) : null} */}
+      
+        {/* </form> */}
     </div>
   );
 }
