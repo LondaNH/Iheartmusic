7a1360bd242bc567644ff08ebeaa549a1a238905
Ellie <ellie@Ellies-MBP.attlocal.net>
2023-05-13 14:20:10 -0700

    package style

--- client/src/pages/Packages.js
+++ client/src/pages/Packages.js
@@ -2,11 +2,12 @@ import React from 'react';
 import PackageMenu from '../components/PackageMenu';
 import PackageList from '../components/PackageList';
 import Cart from '../components/Cart';
+import '../styles/PackageMenu.css'
 
 
 function Packages(){
     return (
-          <div className="container">
+          <div className="pmcontainer">
       <PackageMenu />
       <PackageList />
       <Cart />
